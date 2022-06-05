e32a6117118514cf999d034cf37556c5e775fe9c
Jorge Baez <jibm_82@yahoo.com.mx>
2022-06-04 22:49:56 -0500

    Update images

--- resources/views/home/partials/footer.blade.php
+++ resources/views/home/partials/footer.blade.php
@@ -1,13 +1,13 @@
 <!-- BEGIN FOOTER -->
 <footer id="footer-onepage" class="bg-color">
+    <div class="footer-image-container">
+        <img src="images/slideshow/walking3.jpg"/>
+        <div class="overlay"></div>
+    </div>
     <div class="footer-widget-area">
         <div class="container">
             <div class="row">
                 <div class="col-md-4">
-                    <div class="footer-info left">
-                        Sábado, 19 de noviembre de 2022 a las 5:00 p.m.<br>
-                        Campeche, Campeche
-                    </div>
                 </div>
 
                 <div class="col-md-4">
@@ -19,10 +19,6 @@
                 </div>
 
                 <div class="col-md-4">
-                    <div class="footer-info right">
-                        Tel.: +0012 345 678 910<br>
-                        E-mail: isabella.andrew@example.com
-                    </div>
                 </div>
             </div>
         </div>
@@ -32,7 +28,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-12">
-                    <br/><br/
+                    <br/><br/>
                 </div>
             </div>
         </div>
