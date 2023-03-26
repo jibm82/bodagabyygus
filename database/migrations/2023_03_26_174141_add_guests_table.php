d44426725aef731575859f670a047d5b62db6bd5
Jorge Baez <jibm_82@yahoo.com.mx>
2023-03-26 12:21:56 -0600

    Add table

---
+++ database/migrations/2023_03_26_174141_add_guests_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('guests', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('second_last_name');
+            $table->integer('people');
+            $table->string('country_code')->nullable(true);
+            $table->string('phone')->nullable(true);
+            $table->string('category');
+            $table->boolean('foreigner')->default(false);
+            $table->boolean('ice_breaker')->default(false);
+            $table->boolean('wedding')->default(false);
+            $table->boolean('after_party')->default(false);
+            $table->text('message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('guests');
+    }
+};
